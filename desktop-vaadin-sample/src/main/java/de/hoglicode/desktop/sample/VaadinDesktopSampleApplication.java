cc0c349384be5251cd7ef06f8ea2256d6229bcef
HogliHP <HogliHP@LAPTOP-UCVKNT7P>
2017-10-23 19:54:29 +0200

    Spring boot 1.5.8 inserted spring.factories file

--- desktop-vaadin-sample/src/main/java/de/hoglicode/desktop/sample/VaadinDesktopSampleApplication.java
+++ desktop-vaadin-sample/src/main/java/de/hoglicode/desktop/sample/VaadinDesktopSampleApplication.java
@@ -2,8 +2,10 @@ package de.hoglicode.desktop.sample;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+//@ComponentScan("de.hoglicode.desktop.sample,de.hoglicode.desktop.vaadin")
 public class VaadinDesktopSampleApplication {
 
 	public static void main(String[] args) {
